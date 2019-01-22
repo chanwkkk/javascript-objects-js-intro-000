3e34bb6f0f9b53f16474ab51ea3d513a89e5c781
Yingqi Chen <chanwk18@hotmail.com>
2019-01-22 17:28:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,11 @@ function updatePlaylist(obj, artistName, songTitle){
 }
 
 function removeFromPlaylist(obj, name){
+<<<<<<< HEAD
   delete obj[name];
+=======
+  delete obj.name;
+>>>>>>> 56fdf7e739389b705a36ce8244aa1d68e80a8c14
   return obj;
 
 }
